8fd76511c5b296f81ef6f3e3eb57ba651ffa66c0
rtungol <rarrtungol@gmail.com>
2023-02-28 10:11:17 -0500

    complete ssr exercises

--- 03/03-05_getServerSideProps/.next/static/chunks/webpack.js
+++ 03/03-05_getServerSideProps/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d9a28c6aec050a7e"; }
+/******/ 		__webpack_require__.h = function() { return "ab0213ebf5294ed9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- 03/03-05_getServerSideProps/pages/news/[query].js
+++ 03/03-05_getServerSideProps/pages/news/[query].js
@@ -1,19 +1,36 @@
-import Layout from "../../components/Layout"
-import { search } from "../api"
+import Layout from "../../components/Layout";
+import { search } from "../api";
 
-export default function News() {
-    return(
-      <Layout>
-        <h1>Search</h1>
-      </Layout>
-    )
+export default function News({ results, query }) {
+  return (
+    <Layout>
+      <h1>Search: {query}</h1>
+      <ul>
+        {results.map((result) => {
+          return (
+            <li key={result.uri}>
+              <a href={result.url} target="_blank">
+                {result.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </Layout>
+  );
 }
 
-const API_KEY = "a2Gp0bWGCzefcXBBjzvplAp0nVEuavee"
+const API_KEY = "a2Gp0bWGCzefcXBBjzvplAp0nVEuavee";
 export async function getServerSideProps({ params }) {
   // The value of the `props` key will be
   //  passed to the `Home` component
+  const URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${params.query}&api-key=${API_KEY}`;
+  const results = await search(URL);
+
   return {
-    props: {}
-  }
+    props: {
+      results,
+      query: params.query,
+    },
+  };
 }
