e6785a96b9bdea806a712b798b3f19b1e2025e4d
rtungol <rarrtungol@gmail.com>
2023-02-28 17:40:03 -0500

    loading static images exercise complete

--- 04/04-01-global-css/pages/404.js
+++ 04/04-01-global-css/pages/404.js
@@ -1,12 +1,16 @@
+import Image from "next/image";
+import gif from "../public/not-found.gif";
 import Layout from "../components/Layout";
 export default function NotFound() {
   return (
     <Layout>
       <h1>Not Found</h1>
+      <Image src={gif} alt="not_found"></Image>
       <style jsx>
         {`
           h1 {
             color: red;
+            visibility: hidden;
           }
         `}
       </style>
